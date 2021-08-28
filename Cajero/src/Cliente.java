0bb1902815d8ca376da9b2d35b0c0513191dadae
BMLW <solaror50@gmail.com>
2021-08-27 23:57:20 -0400

    Segundo commit antes de media noche

--- Cajero/src/Cliente.java
+++ Cajero/src/Cliente.java
@@ -5,25 +5,26 @@ public class Cliente {
 	private String nombre;
 	private String codigo;
 	private int pin;
-	private ArrayList<Cuenta> numCuentas = new ArrayList();
+	 ArrayList<Cuenta> numCuentas = new ArrayList<Cuenta>();
 
-	public Cliente(String nombre, String codigo, int pin) {
-		this.nombre = nombre;
-		this.codigo = codigo;
-		this.pin = pin;
-		aniadirCuentas(numCuentas);
+	
+	public Cliente() {
+		
 	}
 
-	public void aniadirCuentas(ArrayList<Cuenta> numCuentas2) {
+	public void aniadirCuentas() {
 		if (getCodigo().equals("jperez")) {
+			this.setPin(3333);
 			Cuenta cnt1 = new Cuenta(111122, "Bolivianos", "Caja de ahorros", 12000);
 			Cuenta cnt2 = new Cuenta(112211, "USD", "Cuenta Corriente", 100);
 			numCuentas.add(cnt1);
 			numCuentas.add(cnt2);
 		} else if (getCodigo().equals("mperez")) {
+			this.setPin(4444);
 			Cuenta cnt1 = new Cuenta(221122, "Bolivianos", "Caja de ahorros", 0);
 			numCuentas.add(cnt1);
 		} else if (getCodigo().equals("cperez")) {
+			this.setPin(3333);
 			Cuenta cnt1 = new Cuenta(111122, "Bolivianos", "Caja de ahorros", 12000);
 			Cuenta cnt2 = new Cuenta(112211, "USD", "Cuenta Corriente", 100);
 			Cuenta cnt3 = new Cuenta(332233, "Bolivianos", "Caja de ahorros", 100000);
@@ -34,6 +35,34 @@ public class Cliente {
 		}
 
 	}
+	
+	public boolean revisarPin(String usuario) {
+		boolean flag=false;
+		if (usuario.equals("jperez")) {
+			if(this.getPin()==3333);
+			return true;
+		} else if (usuario.equals("mperez")) {
+			if(this.getPin()==4444);
+			return true;
+		} else if (usuario.equals("cperez")) {
+			if(this.getPin()==3333);
+			return true;
+		}
+		return flag;
+	}
+	
+	public boolean revisarClientes(String codigoRev) {
+		boolean flag=false;
+		if (getCodigo().equals("jperez")) {
+			flag=true;
+		} else if (getCodigo().equals("mperez")) {
+			flag= true;
+		} else if (getCodigo().equals("cperez")) {
+			flag= true;
+		}
+		return flag;//retorna true si encuentra el nombre y si da false no lo encontro
+	}
+	
 
 	public String getNombre() {
 		return nombre;
--- Cajero/src/MainCajero.java
+++ Cajero/src/MainCajero.java
@@ -1,4 +1,79 @@
+import java.util.Scanner;
 
 public class MainCajero {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String codigo = "";
+		int pin = 0;
+		int contador = 3;// contador que se encarga de que no haya mas de 3 intentos por sesion
+		boolean flag = true;
+		Cliente pruebas = new Cliente();
+		while (flag && contador != 0) {// inicio de sesion
+			System.out.println("Ingrese el codigo de usuario: \n");
+			codigo = sc.nextLine();
+			pruebas.aniadirCuentas();
+			// devuelve true si lo encuentra al cliente false si no
+			if (pruebas.revisarClientes(codigo)) {
+				
+				System.out.println("Ingrese el pin de seguridad: \n");
+				pin = sc.nextInt();
+				// devuelve true si el pin es correcto false si no lo es
+				if (pruebas.revisarPin(codigo)) {
+					flag = false;
+				} else {
+					System.out.println("Intente de nuevo!");
+					contador--;
+				}
+			} else {
+				System.out.println("Intente de nuevo!");
+				contador--;
+			}
+		}
+		if (contador != 0) {
+			System.out.println("\n\n\n\n\n\nSe cerro sesion!");
+		} else {// el flag es false entonces se entro correctamente
+			flag = true;
+			int decision=0;
+			while (flag) {//menu de despliegue
+				System.out.println("\n\n\n\n\n\nEscoja su opcion\n1.) Ver Saldo\n"
+						+ "2.)Retiro\n3.)Deposito\n4.)Salir");
+				decision=sc.nextInt();
+				if(decision>=1||decision<5) {
+					if(decision==1) {
+						saldo(pruebas);
+					}else if(decision==2) {
+						retiro(pruebas);
+					}else if(decision==3) {
+						deposito(pruebas);
+					}else if(decision==4) {
+						flag=false;
+					}
+				}else {
+					System.out.println("\n\nIngrese un numero correcto!");
+				}
+				
+			}//fin de while de menu
+			System.out.println("Se cerro sesion correctamente, hasta luego.");
+		}
 
+	}
+
+	public static void deposito(Cliente pruebas) {
+		
+	}
+
+	public static void retiro(Cliente pruebas) {
+		
+	}
+
+	public static void saldo(Cliente pruebas) {
+		String moneda,tipo;
+		int saldo;
+		for(int i=0;pruebas.numCuentas.size()<i;i++) {
+			moneda=pruebas.numCuentas.get(i).getMoneda();
+			tipo=pruebas.numCuentas.get(i).getTipo();
+			saldo=pruebas.numCuentas.get(i).getSaldo();
+			System.out.println();
+		}
+	}
 }
